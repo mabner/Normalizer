9678adb53ccc027af08dd81e1b615fda626014d5
Marcos Leite <m.abner@live.co.uk>
2024-08-20 22:11:14 -0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Normalizer
+﻿
+namespace Normalizer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Input original text:");
+
+            string oldString = Console.ReadLine();
+
+            string newString = StringNormalizer(oldString);
+
+            Console.WriteLine(newString);
+        }
+
+        private static string StringNormalizer(string? oldString)
+        {
+            throw new NotImplementedException();
         }
     }
 }
